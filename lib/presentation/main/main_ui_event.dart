8c758cc41d5296efeff5589ad81bba20daac98bb
sinbee0402 <osb3476@gmail.com>
2023-07-20 21:36:05 +0900

    Feat: main event, viewModel, state 작성

---
+++ lib/presentation/main/main_ui_event.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'main_ui_event.freezed.dart';
+
+@freezed
+sealed class MainUiEvent<T> with _$MainUiEvent<T> {
+  const factory MainUiEvent.loadNotes() = LoadNotes;
+  const factory MainUiEvent.deleteNote(Note note) = DeleteNote;
+  const factory MainUiEvent.restoreNote(Note note) = RestoreNote;
+}
---
+++ lib/presentation/main/main_view_model.dart
@@ -0,0 +1,54 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_note_app/presentation/main/main_state.dart';
+import 'package:flutter_note_app/presentation/main/main_ui_event.dart';
+
+class MainViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  MainState _state = MainState();
+  MainState get state => _state;
+
+  Note? _recentlyDeletedNote;
+
+  MainViewModel(this.repository);
+
+  void onEvent(MainUiEvent event) {
+    // 26 첫번째 viewModel 작성
+    switch (event) {
+      case LoadNotes():
+        _loadNotes;
+      case DeleteNote():
+        _deleteNote;
+      case RestoreNote():
+        _restoreNote;
+    }
+  }
+
+  Future<void> _loadNotes() async {
+    List<Note> notes = await repository.getNotes();
+    _state = state.copyWith(
+      notes: notes,
+    );
+    notifyListeners();
+  }
+
+  Future<void> _deleteNote(Note note) async {
+    await repository.deleteNote(note);
+    _recentlyDeletedNote = note;
+
+    await _loadNotes();
+  }
+
+  Future<void> _restoreNote() async {
+    if (_recentlyDeletedNote != null) {
+      await repository.insertNote(_recentlyDeletedNote!);
+      _recentlyDeletedNote = null;
+
+      _loadNotes();
+    }
+  }
+}
